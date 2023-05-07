4599c4eb77b66f35b655406dca7b7cbcce53e06e
LukaszKlimas <97245750+LukaszKlimas@users.noreply.github.com>
2023-05-07 15:02:50 +0200

    up 1.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/src/partial/header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch(error => console.log(error));
+});
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
